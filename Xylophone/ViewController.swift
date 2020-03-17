48de9bbabbd6108de30ba4dc77b4bc622ded43a4
Daniel Sutcliffe <dannysutcliffe@hotmail.com>
2020-03-17 16:23:46 +0000

    first commit

---
+++ Xylophone/ViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Angela Yu on 28/06/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+import AVFoundation // Module for audio visual hardware - See Apple Documentation
+
+class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func keyPressed(_ sender: UIButton) {
+        print("Button Pressed")
+        print("Title: \(sender.currentTitle!)") // Optional, forces us to check if any button has no title... we will aways have a title
+        playSound(soundName: sender.currentTitle!)
+        
+        //Reduces the sender's (the button that got pressed) opacity to half.
+        sender.alpha = 0.5
+        
+        //Code should execute after 0.2 second delay.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            //Bring's sender's opacity back up to fully opaque.
+            sender.alpha = 1.0
+        }
+        
+    }
+    
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player?.play()
+    }
+
+    
+//    func playSound() {
+//        guard let url = Bundle.main.url(forResource: "C", withExtension: "wav") else { return }     //
+//
+//        do {
+//            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)      // even in silent mode
+//            try AVAudioSession.sharedInstance().setActive(true)
+//
+//            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+//
+//            guard let player = player else { return }
+//
+//            player.play()
+//
+//        } catch let error {
+//            print(error.localizedDescription)
+//        }
+//    }
+    
+}
